7b3b8f85941aaf9ab7d73363883cc33ba26ba056
Steve Witman <stevewitman@gmail.com>
2015-05-05 21:39:48 -0600

    remove unused sections

--- js/main.js
+++ js/main.js
@@ -1,114 +1,120 @@
 $(document).ready(function() {
 
 
-    /* Scroll hire me button to contact page */
-    $('.hire-me').click(function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500);
-        return false;
+  /* Scroll hire me button to contact page */
+  $('.hire-me').click(function() {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
+  });
+
+  /* For Bootstrap current state on portfolio sorting */
+
+  $('ul.nav-pills li a').click(function(e) {
+    $('ul.nav-pills li.active').removeClass('active')
+    $(this).parent('li').addClass('active')
+  })
+
+  /* portfolio mixitup */
+
+  $(window).load(function() {
+    var $container = $('.grid-wrapper');
+    $container.isotope({
+      filter: '*',
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false
+      }
     });
 
-    /* For Bootstrap current state on portfolio sorting */
+    $('.grid-controls li a').click(function() {
+      $('.grid-controls .current').removeClass('current');
+      $(this).addClass('current');
 
-    $('ul.nav-pills li a').click(function(e) {
-        $('ul.nav-pills li.active').removeClass('active')
-        $(this).parent('li').addClass('active')
-    })
-
-    /* portfolio mixitup */
-
-    $(window).load(function() {
-        var $container = $('.grid-wrapper');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-
-        $('.grid-controls li a').click(function() {
-            $('.grid-controls .current').removeClass('current');
-            $(this).addClass('current');
-
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-    });
-
-
-    /* Magnific Popup */
-    $('.grid-wrapper').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery: {
-            enabled: true
+      var selector = $(this).attr('data-filter');
+      $container.isotope({
+        filter: selector,
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
         }
+      });
+      return false;
     });
+  });
 
 
-    /* Sticky menu */
-    $(".navbar").sticky({
-        topSpacing: 0
+  /* Magnific Popup */
+  $('.grid-wrapper').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  });
+
+
+  /* Sticky menu */
+  $(".navbar").sticky({
+    topSpacing: 0
+  });
+
+
+  /* Scroll spy and scroll filter */
+  $('#main-menu').onePageNav({
+    currentClass: "active",
+    changeHash: false,
+    scrollThreshold: 0.5,
+    scrollSpeed: 750,
+    filter: "",
+    easing: "swing"
+  });
+
+  /* Charts*/
+
+  $('.chart').waypoint(function() {
+    $(this).easyPieChart({
+      barColor: '#3498db',
+      size: '150',
+      easing: 'easeOutBounce',
+      onStep: function(from, to, percent) {
+        $(this.el).find('.percent').text(Math.round(percent));
+      }
     });
+  }, {
+    triggerOnce: true,
+    offset: 'bottom-in-view'
+  });
 
+  /* VEGAS Home Slider */
 
-    /* Scroll spy and scroll filter */
-    $('#main-menu').onePageNav({
-        currentClass: "active",
-        changeHash: false,
-        scrollThreshold: 0.5,
-        scrollSpeed: 750,
-        filter: "",
-        easing: "swing"
-    });
-
-    /* Charts*/
-
-    $('.chart').waypoint(function() {
-        $(this).easyPieChart({
-            barColor: '#3498db',
-            size: '150',
-            easing: 'easeOutBounce',
-            onStep: function(from, to, percent) {
-                $(this.el).find('.percent').text(Math.round(percent));
-            }
-        });
-    }, {
-        triggerOnce: true,
-        offset: 'bottom-in-view'
-    });
-
-
-    /* VEGAS Home Slider */
-	
-	    $('#page-welcome').vegas({
-        slides: [
-            { src: 'img/slider/01.jpg' },
-            { src: 'img/slider/02.jpg' },
-            { src: 'img/slider/03.jpg' },
-			{ src: 'img/slider/04.jpg' }
-        ],
+  $('#page-welcome').vegas({
+    slides: [
+      {src: 'img/slider/IMAG0034a.jpg'},
+      {src: 'img/slider/IMG_2810d.jpg'},
+      {src: 'img/slider/fruita.jpg'},
+      {src: 'img/slider/IMG_4919.jpg'},
+      {src: 'img/slider/IMG_4967.jpg'},
+      {src: 'img/slider/IMG_5034.jpg'},
+      {src: 'img/slider/IMG_5139.jpg'},
+      {src: 'img/slider/IMG_5340.jpg'},
+      {src: 'img/slider/IMG_8481.jpg'},
+      {src: 'img/slider/IMG_1766.jpg'},
+      {src: 'img/slider/IMG_6891.jpg'},
+      {src: 'img/slider/15c.jpg'},
+      {src: 'img/slider/WPbase2.jpg'},
+    ],
     overlay: true
-});
+  });
 
-    $("#vegas-next").click(function() {
-        $('#page-welcome').vegas('next');
-    });
-    $("#vegas-prev").click(function() {
-       $('#page-welcome').vegas('previous');
-    });
-
-
+  $("#vegas-next").click(function() {
+    $('#page-welcome').vegas('next');
+  });
+  $("#vegas-prev").click(function() {
+    $('#page-welcome').vegas('previous');
+  });
 
 });
